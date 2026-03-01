ad1b675cbe845829001c42443907725747ab63d2
ginaa07 <1123150124@global.ac.id>
2026-03-02 00:17:47 +0700

    add List Product get items repositories

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:aplikasicatalog/features/cart/domain/entities/product.dart';
 import 'package:aplikasicatalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartProvider extends ChangeNotifier {
@@ -6,3 +7,10 @@ final CartRepository _repository;
 
 // Dependency Injection melalui constructor
 CartProvider({required CartRepository repository}) : _repository = repository;
+
+List<Product> get items => _repository.getCartItems();
+
+void addItem(Product product) {
+_repository.addItem(product);
+notifyListeners();
+}
