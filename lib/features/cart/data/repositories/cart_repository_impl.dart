c637cb1ba8a04bea9217b29f7224b0ec20f76ac0
ginaa07 <1123150124@global.ac.id>
2026-03-01 23:57:30 +0700

    add removeAllItems and isItemInCart in Cart repositories impl

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -8,7 +8,14 @@ class CartRepositoryImpl implements CartRepository {
   final List<Product> _items = [];
 
   @override
-List<Product> getCartItems() => List.unmodifiable(_items);
-@override
-void addItem(Product product) => _items.add(product);
-@override
+  List<Product> getCartItems() => List.unmodifiable(_items);
+
+  @override
+  void addItem(Product product) => _items.add(product);
+
+  @override
+  void removeAllItems() => _items.clear();
+  
+  @override
+  bool isItemInCart(String productId) => _items.any((p) => p.id == productId);
+}
